ecff9015b913d712332461a1e94c4e5e8b7bdbe6
kanybekd <37711190+kanybekd@users.noreply.github.com>
2020-08-10 23:17:35 -0500

    hw2!

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,12 @@
 // Your JS code is here
+// Forgot how old someone is? Calculate it!
+
+//     Store the current year in a variable.
+//     Store their birth year in a variable.
+//     Calculate their 2 possible ages based on the stored values.
+//     Output them to the screen like so: "They are either NN or NN", substituting the values.
+
+var c_year=2020;
+var b_year=1987;
+
+console.log('They are either ',c_year-b_year,' or ',b_year-c_year);
