82ddca894d4e9faad708563dceebc8c31890b1e1
kanybekd <37711190+kanybekd@users.noreply.github.com>
2020-08-10 23:19:52 -0500

    hw1!

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,8 @@
 // Your JS code is here
+
+var num_of_children=2;
+var partner_name='Clinton';
+var geo_loc='Austin';
+var job_title='Engineer';
+
+document.write('You will be an '+ job_title + ' in '+ geo_loc + ',' + ' and married to '+partner_name+' with ',num_of_children+' kids'  )
