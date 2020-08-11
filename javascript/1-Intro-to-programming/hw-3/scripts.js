6d48267fec7e6caf89c17173c99611ca41ed56a9
kanybekd <37711190+kanybekd@users.noreply.github.com>
2020-08-10 23:30:02 -0500

    hw3!

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,20 @@
 // Your JS code is here
+// 3. The Temperature Converter
+
+// Let's make a program to convert celsius tempatures to fahrenheit and vice versa, so we can complain about the weather with our friends oversees.
+
+// Reminder: To convert celsius to fahrenheit, multiply by 9, then divide by 5, then add 32. To convert fahrenheit to celsius, deduct 32, then multiply by 5, then divide by 9.
+
+// Unicode Characters: To print the degrees symbol in JavaScript, we need to use the write \u00B0 to represent the unicode character for the degress symbol.
+
+//     Store a celsius temperature into a variable.
+//     Convert it to fahrenheit and output "NN°C is NN°F".
+//     Now store a fahrenheit temperature into a variable.
+//     Convert it to celsius and output "NN°F is NN°C."
+
+var celsius=28;
+var fahrenheit=94;
+
+document.write(celsius,'\u00B0','C',' in fahrenheit is ',celsius*9/5+32,'\u00B0','F');
+document.write("<br>");
+document.write(fahrenheit,'\u00B0','F',' in Celsius is ',((fahrenheit-32)*5/9).toFixed(1),'\u00B0','C');
