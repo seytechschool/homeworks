f552bb70e9d7a154a146b3a75bb3213b58ff321b
Kanye09 <kanybek09@yahoo.com>
2020-08-11 02:08:28 -0400

    homeworks for Intro to Programming section

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,16 @@
 // Your JS code is here
+
+// 1. The Fortune Teller
+// Why pay a fortune teller when you can just program your fortune yourself?
+
+// Store the following into variables: number of children, partner's name, geographic location, job title.
+// Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+
+var kids = 3;
+var name = "Jane";
+var loc = "New York";
+var job = "programmer";
+
+var person = name + " is a " + job + ". She lives in " + loc + " and she has a " + kids + " beautiful kids.";
+
+console.log (person);
