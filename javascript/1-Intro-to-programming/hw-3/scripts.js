abc372b37896f3e2e6344e2de5b91f3261ec67ea
FarkhatSaliyev <68262243+FarkhatSaliyev@users.noreply.github.com>
2020-08-11 16:31:02 -0700

    Update scripts.js

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,10 @@
 // Your JS code is here
+var celsius = 24;
+var farenheit = celsius*9/5+32;
+var F = farenheit.toFixed(1);
+console.log(farenheit)
+
+var farenheit = 65;
+var celsius = (farenheit-32)*5/9;
+var C = celsius.toFixed(1);
+console.log(C)
