ec4991cd0c6367ccb5c88117a037ff7fa62f1e3f
tal-dev <talgat.abykeev@gmail.com>
2020-08-10 22:59:50 -0500

    hw-2

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,11 @@
 // Your JS code is here
+
+
+// Version 1
+var currentYear = 2020;
+var birthYear = 1990;
+var age = currentYear - birthYear;
+console.log(`They are either ${age} or ${age - 1}`)
+
+// Vestion 2. Function
+const ageCalculator = (currentYear, birthYear) => `They are either ${currentyear-birthyear} or ${currentyear-birthyear - 1}`;
