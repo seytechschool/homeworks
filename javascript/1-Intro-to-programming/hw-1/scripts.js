d4884ec7d345e8cf832dee9bb34b048752fa796a
Rakhat Mamytova <67878689+RakhatMamyt@users.noreply.github.com>
2020-08-11 15:57:44 -0500

    Update scripts.js

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,6 @@
 // Your JS code is here
+var numberChildren=3
+var partnerName= 'John'
+var location='California'
+var jobName='Software Engineer'
+var finalLine='You will be a'+' '+jobName+' '+'in'+' '+location+', and married to'+' '+partnerName+ ' '+'with'+' '+numberChildren+' '+'kids
