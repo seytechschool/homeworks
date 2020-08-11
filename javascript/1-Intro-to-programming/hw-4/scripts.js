cef1a47584fc1bbefd7dfa9ec0fb5db8bf1133ac
Askat <askatbakyev@gmail.com>
2020-08-11 00:32:41 -0400

    hw-4

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+var random = Math.floor(Math.random()*101);
+var degreeIcon = '\u00B0';
+var farenheit = random;
+var farenheitToCelsius = ((farenheit - 32) * 5 / 9).toFixed(0);
+
+console.log('It is ' + farenheit + degreeIcon + 'F today. That"s '  + farenheitToCelsius + degreeIcon + 'C');
