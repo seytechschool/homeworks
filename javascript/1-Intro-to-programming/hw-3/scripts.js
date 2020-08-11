6d67e263c4d9e7c8acfd9c2e4666e45c8725b4df
Azamat Gaparov <pennphil.ttzf@gmail.com>
2020-08-11 01:47:50 -0400

    small change in intro to programming - hw-3/script.js

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,13 +1,13 @@
 // Your JS code is here
 // Convert Celsius temperatures to Fahrenheit
-var celsius = 30; // 30 degrees in celsius
+var celsius = 30; // X degrees in celsius
 var fahrenheit = (celsius * 9 / 5) + 32;
 
 console.log(celsius + ' degrees in celsius is equal to ' + fahrenheit + ' degrees in fahrenheit');
 
 
 // Convert Fahrenheit temperatures to Celsius
-var fahrenheit = 54; // 54 degrees in fahrenheit
+var fahrenheit = 54; // X degrees in fahrenheit
 
 var celsius = (fahrenheit - 32) * 5 / 9;
 
