735e8ade852e1a3991e97b3ec09994d31ca06934
tamerlan <hudojnik.1993@gmail.com>
2020-08-11 18:20:38 -0400

    first commit

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,10 @@
 // Your JS code is here
+
+var celsius = 29;
+var fahrenheit = (celsius*9)/5+32;
+console.log(celsius + '\u00B0 Celsious is ' + fahrenheit + '\u00B0 Fahrenheit')
+
+
+var fahrenheit = 66;
+var selsius = (fahrenheit-32)*5/9;
+console.log(fahrenheit + '\u00B0 Fahrenheit is ' +  celsius + '\u00B0 Celsious')
