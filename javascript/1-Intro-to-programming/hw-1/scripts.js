681da1b5607bb856753e015bd5c1bd4816f93eca
Azamat Gaparov <pennphil.ttzf@gmail.com>
2020-08-11 01:14:02 -0400

    finished introduction to programming - hw-1

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,2 +1,9 @@
 // Your JS code is here
-var x = 10;
+var num_children = 3;
+var partner_name = 'Julia Roberts';
+var geo_location = 'Italy';
+var job_title = 'Frontend Engineer'
+
+'You will be a ' + job_title + ' in ' + geo_location + ', and married to ' + partner_name + ' with ' + num_children + ' kids'
+
+// console.log('You will be a ' + job_title + ' in ' + geo_location + ', and married to ' + partner_name + ' with ' + num_children + ' kids')
