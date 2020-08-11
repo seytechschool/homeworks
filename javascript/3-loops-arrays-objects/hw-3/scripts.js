66d8a609e1dabfe187cfc123d45e819261fafe40
Askat <askatbakyev@gmail.com>
2020-08-11 03:31:18 -0400

    hw-3-loops

--- javascript/3-loops-arrays-objects/hw-3/scripts.js
+++ javascript/3-loops-arrays-objects/hw-3/scripts.js
@@ -1,1 +1,41 @@
 // Your JS code is here
+var books = [
+  {
+    title: 'Pinocchio',
+    author: 'Carlo Collodi',
+    isRead: true,
+  },
+   {
+    title: 'The Little Prince',
+    author: 'Antoine de Saint-Exupery',
+    isRead: true,
+  },
+   {
+    title: 'War and Piece',
+    author: 'Leo Tolstoy',
+    isRead: false,
+  },
+   {
+    title: 'The Idiot',
+    author: 'Fyodor Dostoevsky',
+    isRead: false,
+  }
+]
+
+// Version 1
+books.forEach(function(book) {
+  if(book.isRead) {
+    console.log('You already read ' + '"' + book.title + '"' + ' by ' + book.author)
+  } else {
+     console.log('You still need to read ' + '"' + book.title + '"' + ' by ' + book.author)
+  }
+})
+
+// Version 2
+books.forEach((book) => {
+  if(book.isRead) {
+    console.log(`You already read "${book.title}" by ${book.author}`)
+  } else {
+     console.log(`You still need to read "${book.title}" by ${book.author}`)
+  }
+})
