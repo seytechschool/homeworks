20c0ee013a0abed0d2f242e769ee79cdc388072b
maxburkanov <40704457+maxburkanov@users.noreply.github.com>
2020-08-11 00:43:06 -0400

    Update scripts.js

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,6 @@
 // Your JS code is here
+function converter(f) {
+
+  console.log(`It is ${f}\u00B0F today, That's ${Math.ceil((f - 32) * 5 / 9)}\u00B0C `)
+}
+converter( Math.ceil(Math.random()*100) )
