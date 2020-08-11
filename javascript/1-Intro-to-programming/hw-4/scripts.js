418c875dabe02994f08eb930a80303e1a3904015
Azamat Gaparov <pennphil.ttzf@gmail.com>
2020-08-11 02:39:51 -0400

    finished introduction to programming - hw-4

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,14 @@
 // Your JS code is here
+// Convert Celsius temperatures to Fahrenheit
+var celsius = Math.floor(Math.random()*101); // Random temperatures between 0-100 degrees in celsius
+var fahrenheit = (celsius * 9 / 5) + 32;
+
+console.log(celsius + ' degrees in celsius is equal to ' + fahrenheit.toFixed(0) + ' degrees in fahrenheit');
+
+
+// Convert Fahrenheit temperatures to Celsius
+var fahrenheit = Math.floor(Math.random()*101); // Random temperatures between 0-100 degrees in fahrenheit
+
+var celsius = (fahrenheit - 32) * 5 / 9;
+
+console.log(fahrenheit + ' degrees in fahrenheit is equal to ' + celsius.toFixed(0) + ' degrees in celsius');
