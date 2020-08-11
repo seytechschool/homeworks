403b6ad200cb16741d728a05f3c5fa44dbdec277
Askat <askatbakyev@gmail.com>
2020-08-11 02:48:54 -0400

    hw-6-function

--- javascript/2-functions/hw-6/scripts.js
+++ javascript/2-functions/hw-6/scripts.js
@@ -1,1 +1,13 @@
 // Your JS code is here
+function fixStart(str) {
+  var firstChar = str.slice(0,1);
+  var restChar = str.slice(1)
+  var newStr = restChar;
+  for(var i = 0; i < restChar.length; i++) {
+    newStr = newStr.replace(firstChar, '*');
+  }
+   
+  return firstChar + newStr;
+}
+
+fixStart('gugguggguggguggg')
