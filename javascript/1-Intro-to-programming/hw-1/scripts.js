b18b53fc18dd336d6c197c374fb729bbc9de8a48
FarkhatSaliyev <68262243+FarkhatSaliyev@users.noreply.github.com>
2020-08-11 16:03:23 -0700

    Update scripts.js

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,2 +1,7 @@
-// Your JS code is here
-var x = 10
+var numberOfchildren = 2;
+var partnerName = "Aizhan";
+var location = "San Diego";
+var jobTitle = "Full Stack Engineer";
+var fortune ="You will be a"+" "+jobTitle+" in"+" "+location+" and married to"+" "+partnerName+" with"+" "+numberOfchildren+" kids.";
+
+console.log(fortune)
