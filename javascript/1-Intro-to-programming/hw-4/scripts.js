33a9a96d322c376b0f571e0b501460bda1fb3750
tal-dev <talgat.abykeev@gmail.com>
2020-08-11 11:13:18 -0500

    completed intro to programming

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,3 @@
 // Your JS code is here
+var randomNumber = Math.floor(Math.random() * (100 - 0) + 0);
+console.log(`It is ${randomNumber}\u00B0C today. That's ${randomNumber * 9 / 5 + 32}\u00B0F}`)
