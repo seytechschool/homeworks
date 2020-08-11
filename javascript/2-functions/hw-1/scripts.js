0d46b8db130ad48db2206de76ec3f3014fd2b2a1
Askat <askatbakyev@gmail.com>
2020-08-11 00:41:35 -0400

    hw-1-functions

--- javascript/2-functions/hw-1/scripts.js
+++ javascript/2-functions/hw-1/scripts.js
@@ -1,1 +1,9 @@
 // Your JS code is here
+function tellFortune(numOfChild, partnerName, location, jobTitle) {
+  return 'You will be a ' + jobTitle + ' in ' + location + ', and married to ' + partnerName + ' with ' + numOfChild + ' kids.';
+}
+
+
+console.log(tellFortune(3, 'Eliza', 'Poland', 'doctor'));
+console.log(tellFortune(2, 'Mark', 'Dublin', 'teacher'));
+console.log(tellFortune(2, 'Hannah', 'Miami', 'police'));
