50fa265fb41f6b4f4705a27140d4c156accd316c
Askat <askatbakyev@gmail.com>
2020-08-11 00:48:14 -0400

    hw-2-functions

--- javascript/2-functions/hw-2/scripts.js
+++ javascript/2-functions/hw-2/scripts.js
@@ -1,1 +1,12 @@
 // Your JS code is here
+function biggerNum(num1, num2) {
+  if(num1 > num2) {
+    return num1;
+  } else if(num1 < num2) {
+    return num2;
+  } else {
+    return 'numbers are equal or cannot be compared.'
+  }
+}
+
+console.log(biggerNum(0, 0))
