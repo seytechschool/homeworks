046ed940cc82092e2adbea7aaa40827283901b2c
Askat <askatbakyev@gmail.com>
2020-08-11 02:04:09 -0400

    hw-5-functions

--- javascript/2-functions/hw-5/scripts.js
+++ javascript/2-functions/hw-5/scripts.js
@@ -1,1 +1,12 @@
 // Your JS code is here
+function mixUp(str1, str2) {
+  var firstTwoCharOfStr1 = str1.slice(0, 2);
+  var restCharOfStr1 = str1.slice(2);
+  var firstTwoCharOfStr2 = str2.slice(0, 2);
+  var restCharOfStr2 = str2.slice(2);
+  var result1 = firstTwoCharOfStr2 + restCharOfStr1;
+  var result2 = firstTwoCharOfStr1 + restCharOfStr2;
+  return result1 + ' ' + result2;
+}
+
+mixUp('mix', 'pod');
