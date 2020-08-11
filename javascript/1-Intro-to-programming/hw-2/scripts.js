4947ef0e20f54fca9263e775108c492304d4cdbc
Aidana Khankel <aidanakhankeldieva91@gmail.com>
2020-08-11 18:55:18 -0500

    completed homework from 1 -4

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,8 @@
 // Your JS code is here
+
+var currentYear = 2020;
+var yearOfBirth = 1991;
+
+var currentAge = currentYear - yearOfBirth;
+
+console.log(`I'm ${currentAge} years old`);
--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,27 @@
 // Your JS code is here
+
+var celsius;
+var fahrenheit;
+var temp;
+var cToF;
+var fToC;
+var unicode= "\u00B0";
+var fahrenheitSymbol = "F";
+var celsiusSymbol = "C";
+
+function enterCelsius(celsius){
+temp = celsius;
+cToF = temp * 9 / 5 + 32;
+console.log()
+console.log(`${celsius}${unicode}${celsiusSymbol} is ${cToF}${unicode}${fahrenheitSymbol}`);
+}
+
+function enterFahrenheit(fahrenheit){
+temp = fahrenheit;
+fToC = (temp - 32) * 5 / 9;
+console.log(`${fahrenheit}${unicode}${fahrenheitSymbol} is ${fToC}${unicode}${celsiusSymbol}`);
+}
+
+// random numbers from 1 -10
+enterCelsius(Math.floor((Math.random() * 10) + 1));
+enterFahrenheit(Math.floor((Math.random() * 10) + 1));
