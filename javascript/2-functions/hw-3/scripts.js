62dbae87c440cfee0559a04058e76d26d37b8a97
Askat <askatbakyev@gmail.com>
2020-08-11 00:59:24 -0400

    hw-3-functions

--- javascript/2-functions/hw-3/scripts.js
+++ javascript/2-functions/hw-3/scripts.js
@@ -1,1 +1,12 @@
 // Your JS code is here
+function pluralize(num, animal) {
+  if(num > 1) {
+    return num + ' ' + animal + 's';
+  } else if(animal === 'goose') {
+    return num + ' ' + 'geese';
+  } else {
+    return num + ' ' + animal;
+  }
+}
+
+console.log(pluralize(10, 'cat'));
