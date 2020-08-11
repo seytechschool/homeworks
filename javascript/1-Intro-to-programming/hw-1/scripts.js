72bf2b40255a05a572f3e9f48b9846b1aef8289a
Askat <askatbakyev@gmail.com>
2020-08-10 23:21:26 -0400

    hw-1

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+var numbOfChildren = 2;
+var partnerName = 'Emily';
+var location = 'Italy';
+var jobTitle = 'teacher';
+
+console.log('You will be a' + ' ' + jobTitle + ' ' + 'in' + ' ' + location + ', and married to' + ' ' + partnerName + ' ' + 'with' + ' ' + numbOfChildren + ' ' + 'kids')
