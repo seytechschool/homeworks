5a96fcc26019c0e2fb5ec3acd00b6dbc2354df7f
Askat <askatbakyev@gmail.com>
2020-08-11 00:15:25 -0400

    hw-3

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,9 @@
 // Your JS code is here
+var celsius = 24;
+var celsiusToFarenheit = (celsius * 9 / 5 + 32).toFixed(0);
+var degreeIcon = '\u00B0';
+console.log(celsius + degreeIcon + 'C is ' + celsiusToFarenheit + degreeIcon + 'F');
+
+var farenheit = 81;
+var farenheitToCelsius = ((farenheit - 32) * 5 / 9).toFixed(0);
+console.log(farenheit + degreeIcon + 'F is ' + farenheitToCelsius + degreeIcon + 'C');
