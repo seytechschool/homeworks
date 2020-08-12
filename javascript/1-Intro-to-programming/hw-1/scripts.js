6536d0be00539df5876a75cd4b390602b2490885
Rustam Ermatov <rustam.22.97@gmail.com>
2020-08-12 17:29:11 +0600

    homework01

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,15 @@
 // Your JS code is here
+var numChildren = Math.random() * (5);
+var age = (Math.random() * (100-23) + 23);
+var name = "Mika";
+var place = "Earth";
+
+var nAge = parseInt(age);
+var nKid = parseInt(numChildren)
+
+
+console.log("I am Rustam and I am " + nAge + " years old and I have " 
+            + nKid + " kids and I am married to " + name + " And, we live on " + place + " !!!");
+
+
+
