bd2a03fedbc35a61eade81fcba4e79360055a40d
Askat <askatbakyev@gmail.com>
2020-08-11 22:41:02 -0400

    hw-4-js-DOM
    countTag function created

--- javascript/4-intro-to-dom/hw-4/scripts.js
+++ javascript/4-intro-to-dom/hw-4/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+function countTags(tag) {
+  var arrOfTag = document.querySelectorAll(tag);
+  console.log('There are ' + arrOfTag.length +  ' tags of type ' + tag + ' on the page');
+}
+
+countTags('p');
