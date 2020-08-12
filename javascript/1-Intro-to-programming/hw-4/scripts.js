2440c7cd121f97e3edc585d88ba8380286c87777
Atay Djunusaliev <adjunusaliev@gmail.com>
2020-08-12 01:28:43 -0700

    hw4

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,12 @@
 // Your JS code is here
+
+var celsius = Math.floor(Math.random() * 101); 
+var celsius_to_fahrenheit = ((celsius * 9) / 5) + 32;
+var degreeIcon = `\u00B0`
+console.log(celsius + degreeIcon + 'C' + ' ' + 'is now' + ' '+celsius_to_fahrenheit + degreeIcon + 'F');
+
+
+var fahrenheit = Math.floor(Math.random() * 101); 
+var fahrenheit_to_cecelsius = ((fahrenheit - 32) * 5) / 9;
+var degreeIcon = `\u00B0`
+console.log(fahrenheit + degreeIcon + 'F' + ' ' + 'is now' + ' '+fahrenheit_to_cecelsius + degreeIcon + 'C'); 
