54258e691ac600ace2f6e92a13e7d30921cf4d9e
maxburkanov <40704457+maxburkanov@users.noreply.github.com>
2020-08-12 18:34:51 -0400

    HW4

--- javascript/2-functions/hw-4/scripts.js
+++ javascript/2-functions/hw-4/scripts.js
@@ -1,1 +1,23 @@
-// Your JS code is here
+function squareNumber(i) {
+  return i**2
+}
+function halfNumber(a) {
+  return a / 2;
+  console.log(`Half of ${a} is ${a/2}`)
+}
+function percent (one,two) {
+  return two*one/100;
+  console.log(`${one} is ${two*one/100}% of ${two}`) 
+}
+function areaCircle(r) {
+  let res =  Math.PI *r*r
+  return res.toFixed(2)
+}
+function total(tot) {
+  let resHalf = halfNumber(tot)  
+  let sq = squareNumber(resHalf)
+  let circ = areaCircle(sq)
+  let percentage = percent(circ, sq)
+  return percentage
+}
+ total(6)
