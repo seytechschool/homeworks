f295c284dab810a3d99c328abc94da4797f1262f
Atay Djunusaliev <adjunusaliev@gmail.com>
2020-08-12 00:31:11 -0700

    hw2

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,6 @@
-// Your JS code is here
+// Your JS code is here 
+
+var currentYear = 2020;
+var myBirthYear = 1987;
+var currentAge = currentYear - myBirthYear;
+console.log('My current age is' + ' '+ (currentYear - myBirthYear) + ' ' + 'years old');
