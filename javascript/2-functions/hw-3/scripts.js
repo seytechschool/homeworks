787247eb00bc9aceca56afecac07de4c36b38778
kanybekd <37711190+kanybekd@users.noreply.github.com>
2020-08-11 23:31:42 -0500

    functions-3!

--- javascript/2-functions/hw-3/scripts.js
+++ javascript/2-functions/hw-3/scripts.js
@@ -1,1 +1,18 @@
 // Your JS code is here
+
+function pluralize(a,b){
+  if (a==1){
+    console.log(a,b)
+  }
+  else if (a>1){
+    console.log (a,b+'s')
+  }
+
+  else{
+    console.log('uncountable # of',b)
+  }
+}
+
+pluralize(2,'horse')
+pluralize(1,'turtle')
+pluralize(0,'goat')
