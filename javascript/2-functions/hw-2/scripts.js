d36f23856d253719ee775843c9f3dfd37947e89e
tamerlan <hudojnik.1993@gmail.com>
2020-08-12 17:26:59 -0400

    tamerlan, function home works done

--- javascript/2-functions/hw-2/scripts.js
+++ javascript/2-functions/hw-2/scripts.js
@@ -1,1 +1,14 @@
 // Your JS code is here
+function Num(idOne,idTwo) {
+  if (idOne > idTwo) {
+    return (
+      idOne
+    )
+  } else {
+    return (
+      idTwo
+    )
+  }
+}
+var number = Num(Math.random(), Math.random());
+console.log(number)
--- javascript/2-functions/hw-3/scripts.js
+++ javascript/2-functions/hw-3/scripts.js
@@ -1,1 +1,19 @@
 // Your JS code is here
+
+
+function calc(num, txt) {
+  if (num = 34 && txt != 'groups') {
+    return
+      console.log('option one is true')
+  } else if (num != 34 || txt = 'peoples') {
+    return
+      console.log('option two is true')
+  } else if (num = 534 || txt = 'students') {
+    return
+      console.log('option three is true')
+  }
+}
+calc(34, 'students')
+
+
+// option three is true because if (num = 534 or txt = 'students') students = students
