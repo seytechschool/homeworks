31fd5f1ecca83bac6ac4b1a02af7c1d2dd7f878a
Askat <askatbakyev@gmail.com>
2020-08-12 00:42:37 -0400

    hw-methods

--- javascript/6-methods/scripts.js
+++ javascript/6-methods/scripts.js
@@ -1,1 +1,28 @@
 // Your JS code is here
+// Task #1
+var txt = "Hello World";
+var index = txt.indexOf('World'); 
+console.log(index)
+
+// Task #2
+var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+var newArr = fruits.splice(1,2);
+console.log(fruits)
+
+// Task #3
+var girls = ["Cecilie", "Lone"];
+var boys = ["Emil", "Tobias", "Linus"];
+var boysAndGirls = girls.concat(boys);
+console.log(boysAndGirls);
+
+// Task #4
+var user = {
+  fistname: 'John',
+  lastname: 'Doe',
+  age: 29,
+  location: 'NY'
+}
+
+for(value in user) {
+  console.log(value + ' : ' + user[value])
+}
