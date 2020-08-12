973d0f53c3e18a2e6eafb256b4d0ada24bbf094e
Nurzhan Nursultanov <nursultanov.nurzhan@gmail.com>
2020-08-13 08:13:27 +1200

    Completed HW1 of JS Intro

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,18 @@
 // Your JS code is here
+var numChildren = 2;
+var wifeName = 'Anna';
+var location = 'New Zealand';
+var jobTitle = 'Research Engineer';
+
+var fortuneTeller =
+  'I am a ' +
+  jobTitle +
+  ' in ' +
+  location +
+  ', and married to ' +
+  wifeName +
+  ' with ' +
+  numChildren +
+  ' kids.';
+
+console.log(fortuneTeller);
