a855c03032c9cf99857c3eeac86de8789032e16d
Rustam Ermatov <rustam.22.97@gmail.com>
2020-08-12 18:53:36 +0600

    hw1

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,5 @@
 // Your JS code is here
+var randomF = Math.random() * 100;
+console.log("Random Farenheit number is: " + randomF);
+var cel = (randomF - 32) * 5 / 9;
+console.log("It will be: " + cel + " Celsius." )
