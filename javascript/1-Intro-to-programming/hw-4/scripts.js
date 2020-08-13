710c0aef3f5f467fa5a3b626c55f39be912435f8
Eliza Amanbaeva <eliza.amanbaeva@gmail.com>
2020-08-13 22:52:41 +0200

    did hw

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+var  fahrenheit = Math.floor(Math.random()*100)
+console.log('It is '+fahrenheit+`\u00B0`+'F'+' today.'+" That's "+Math.floor((fahrenheit - 32) * 5 / 9)+`\u00B0`+'C.')
+
+
+
+
