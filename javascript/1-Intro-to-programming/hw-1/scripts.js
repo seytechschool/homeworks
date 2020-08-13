fe0aa183d7d33e206ddf99dce2164700b56a8a21
Ulanbek <ulan312@gmail.com>
2020-08-13 19:16:25 -0400

    js-ulanbek

---
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -0,0 +1,9 @@
+// Your JS code is here
+// The Fortune Teller
+var x = 'a teacher';
+var y = 'New York';
+var z = 'Lucy';
+var n = 2;
+var fortune= 'You will be '+x +' in '+y+',' 
++'and married to '+z+' with '+n +' kids.';
+console.log(fortune);
---
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -0,0 +1,4 @@
+// Your JS code is here
+var farenheitConverter = Math.floor(Math.random() * 101);
+var celcius = (farenheitConverter - 32)*5/9;
+console.log('It is '+farenheitConverter+'\u00B0F today. That is '+celcius+'\u00B0C.');
