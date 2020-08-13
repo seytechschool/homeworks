4ff05d45a9d7ffdd1034cf2846c98771679ea6fb
Ulanbek <ulan312@gmail.com>
2020-08-13 19:09:54 -0400

    fist commit

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++
@@ -1,7 +0,0 @@
-// Your JS code is here
-// The Age Calculator
-var currentYear=2020;
-var birthYear=1989;
-var firstPossibleAge=currentYear-birthYear;
-var secondPossibleAge=currentYear-birthYear-1;
-console.log('They are either '+firstPossibleAge+' or '+secondPossibleAge);
--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++
@@ -1,8 +0,0 @@
-// Your JS code is here
-var celciusConverter= 32;
-var farenheit = celciusConverter * 9/5 + 32;
-console.log(celciusConverter+'\u00B0C is '+farenheit+'\u00B0F');
-
-var farenheitConverter = 89.6;
-var celcius = (farenheitConverter - 32)*5/9;
-console.log(farenheitConverter+'\u00B0F is '+celcius+'\u00B0C');
