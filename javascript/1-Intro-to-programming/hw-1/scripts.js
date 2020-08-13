115dc720ad2ec8360c69d1bca95f825e9388949a
Ulanbek <ulan312@gmail.com>
2020-08-13 01:26:25 -0400

    hw1

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -4,30 +4,6 @@ var x = 'a teacher';
 var y = 'New York';
 var z = 'Lucy';
 var n = 2;
-var fortune= 'You will be' + ' ' + x + ' ' + 'in' + ' ' + y + ',' 
-+ 'and married to'+ ' ' + z + ' ' + 'with' + ' ' + n + ' kids.';
+var fortune= 'You will be '+x +' in '+y+',' 
++'and married to '+z+' with '+n +' kids.';
 console.log(fortune);
-
-// The Temperature Calculator
-var a= 32;
-var b = a * 9/5 + 32 + '\u00B0';
-console.log(b);
-
-var farenheit = 76;
-var celcius = (farenheit - 32)*5/9+'\u00B0';
-console.log(celcius);
-
-// Age calculator
-
-var currentYear = 2020;
-var birthYearA= 1989;
-var birthYearB = 1995;
-var possibleAge = 'They are either' + ' ' + (currentYear - 
-birthYearA) + ' ' + 'or' + ' ' + (currentYear - birthYearB);
-console.log(possibleAge);
-
-// Challenge question
-
-var a= Math.random();
-var b = 'It is' + ' ' + a + '\u00B0 Farenheit today.' + 'That is' + ' '+ (a * 9/5 + 32 + '\u00B0');
-console.log(b);
