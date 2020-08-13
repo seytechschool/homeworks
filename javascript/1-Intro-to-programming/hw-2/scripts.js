f2a7249fddde4ba5c0bec4e1dbc9fdd35ade4704
Myshyk <50039080+AidanaKhankeldieva@users.noreply.github.com>
2020-08-12 19:55:34 -0500

    Update scripts.js

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -4,5 +4,8 @@ var currentYear = 2020;
 var yearOfBirth = 1991;
 
 var currentAge = currentYear - yearOfBirth;
-
 console.log(`I'm ${currentAge} years old`);
+
+
+var currentAgePrediction = currentYear - yearOfBirth -1;
+console.log(`I'm ${currentAgePrediction} years old`);
