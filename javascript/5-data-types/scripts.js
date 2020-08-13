84ca20261ef1752cda57b14c47cc3645cea3a722
kanybekd <37711190+kanybekd@users.noreply.github.com>
2020-08-13 00:55:23 -0500

    even-odd!

--- javascript/5-data-types/scripts.js
+++ javascript/5-data-types/scripts.js
@@ -1,1 +1,12 @@
 // Your JS code is here
+var even=0
+var odd=0
+for (var i=0; i<20;i++){
+  if (i%2==0){
+    even+=1
+  }
+  else{
+    odd+=1
+  }
+}
+console.log('there are ',even,'even and',odd,'odd numbers')
