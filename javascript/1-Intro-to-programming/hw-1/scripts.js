ac595ffa5d84cb1db0a866b9368bf48426508109
Eliza Amanbaeva <eliza.amanbaeva@gmail.com>
2020-08-14 03:37:06 +0200

    intro-to-js completed

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -3,4 +3,4 @@ var childrencount = 2
 var husband = 'tynar'
 var city = 'warsaw'
 var position = 'software engineer'
-console.log('You will be a ' + position + ' in ' + city + ', and married to ' + husband + ' with ' + childrencount + ' kids.' )
+console.log('You will be a ' + position + ' in ' + city + ', and married to ' + husband + ' with ' + childrencount + ' kids.')
--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+var celsius = 30;
+var fahrenheit = 32;
+
+console.log(celsius+`\u00B0`+'C'+' is '+ ((celsius*9)/5+32)+ `\u00B0`+'F')
+console.log(fahrenheit+`\u00B0`+'F'+' is '+((fahrenheit-32)*5/9)+`\u00B0`+'C')
+
