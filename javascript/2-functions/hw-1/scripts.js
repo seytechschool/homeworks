b4ef4e475c0f63985d214fe538217a4bb19b49f3
Rustam Ermatov <rustam.22.97@gmail.com>
2020-08-14 07:17:55 +0600

    hw2

--- javascript/2-functions/hw-1/scripts.js
+++ javascript/2-functions/hw-1/scripts.js
@@ -1,1 +1,6 @@
 // Your JS code is here
+// Your JS code is here
+function tellFortune(numKid, namePartner, location, job){
+    return("You will be " + location + " as " + job + " with " + namePartner + " with " + numKid + " kids");
+}
+tellFortune();
