b885f9344de12695242a26dbd384a7acb79efb3c
Azamat Gaparov <pennphil.ttzf@gmail.com>
2020-08-13 22:20:12 -0400

    funtions hw-6 - completed

--- javascript/2-functions/hw-6/scripts.js
+++ javascript/2-functions/hw-6/scripts.js
@@ -1,1 +1,12 @@
 // Your JS code is here
+function fixStart(str){
+    var num_chars = str.length;
+    var first_char = str.slice(0,1);
+    var second_part = str.slice(1,num_chars);
+  
+    var new_repl_str = second_part.replace(/first_char/g, '*');
+    console.log(first_char + new_repl_str);
+  }
+  
+  fixStart('babble');
+  
