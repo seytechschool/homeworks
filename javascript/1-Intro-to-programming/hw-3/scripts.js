f1c7a8055e195b913e3c0a20e82b73672d776cbd
tal-dev <talgat.abykeev@gmail.com>
2020-08-14 15:14:24 -0500

    intro-to-programming-tal-dev

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,4 @@
 // Your JS code is here
+const convertCelsiusToFahrenheit = celsius => `${celsius}\u00B0C is ${celsius * 9 / 5 + 32}\u00B0F`;
+
+const convertFahrenheitToCelsius = fahrenheit => `${fahrenheit}\u00B0F is ${(fahrenheit - 32) * 5 / 9}\u00B0C`;
