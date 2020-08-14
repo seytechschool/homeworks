7f1818e0be1e8db7714e5fdb4cec21590837e3bf
Askat B <Askat@MacBook-Pro-2.home>
2020-08-13 21:20:50 -0400

    hw-1 intro to js

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,17 @@
 // Your JS code is here
+var children = 3;
+var partnerName = "Joe";
+var geoLocation = "Iceland";
+var jobTitle = "teacher";
+
+document.write(
+  "You will be a " +
+    jobTitle +
+    " in " +
+    geoLocation +
+    ", and married to " +
+    partnerName +
+    " with " +
+    children +
+    " kids."
+);
