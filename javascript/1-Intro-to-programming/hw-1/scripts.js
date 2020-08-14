79bc8ce0ce5a2fd269b4c5fc11175a3f7903e809
Viktoriia Leseiko <ViktoriiaL.@Viktoriias-Air.attlocal.net>
2020-08-13 23:47:04 -0500

    functions-Vikysha1

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,9 @@
 // Your JS code is here
+var numberOfChildren = 3;
+var partnerName = 'Jason Derulo'
+var geoLocation = 'Bora-Bora'
+var jobTitle = 'fashion influencer'
+
+var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' +
+   partnerName + ' ' + ' with ' + numberOfChildren + ' kids.';
+console.log(future);
--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,5 @@
 // Your JS code is here
+var currentYear = 2020;
+var birthYear = 1995;
+var age = currentYear - birthYear;
+console.log('I will be either ' + age + ' or ' + (age - 1));
