38381c59884ae46c4752da2b24a887c1322ec191
Farkhat Saliyev <saliyev.farkhat@gmail.com>
2020-08-13 18:47:12 -0700

    slightly updated

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,5 +1,6 @@
 // Your JS code is here
 var currentYear = "2020"
     var birthYear = "1994" //Put your date of birth
-    var age ="They are either "+ (currentYear - birthYear)+ " or "+(currentYear - birthYear)+" ,substituting the values."
+    var age ="They are either "+ (currentYear - birthYear)+ " or "+(currentYear - birthYear-1)+" ,substituting the values."
     console.log(age)
+    console.log(age-1)
