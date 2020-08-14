198310619942456b489152e76e2a8b623fdc798f
SholaNur <shoola.nur@gmail.com>
2020-08-13 23:46:45 -0700

    intro-to-js-hw-complited

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,7 @@
-// Your JS code is here
+var celTemp = 25;
+var celTempInF = (celTemp * 9) / 5 + 32;
+console.log(celTemp + 'C is ' + celTempInF + 'F');
+
+var frenTemp = 86;
+var frenTempInC = (frenTemp - 32) / 9 * 5;
+console.log(frenTemp + 'F is ' + frenTempInC + 'C');
