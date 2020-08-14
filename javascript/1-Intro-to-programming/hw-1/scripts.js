18369d9ccd239a1f78a5201e629d685ee4c3f241
Atay Djunusaliev <adjunusaliev@gmail.com>
2020-08-13 23:54:30 -0700

    intro-to-js-hw1-hw4

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,8 @@
 // Your JS code is here
+var myself = 'Atay'
+var numberOfChildren = 1;
+ var partnerName = 'Bella';
+ var geographiclocation = 'Mountain View';
+ var jobtitle = 'Software Engineer';
+ var fortune =  myself + ' ' + 'will be a' + ' ' + jobtitle + ' ' + 'in' + ' ' + geographiclocation + ', and married to' + ' ' + partnerName + ' ' + 'with' + ' ' + numberOfChildren + ' ' + 'kid.';
+ console.log(fortune)
