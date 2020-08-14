28554da55fc7d7496bcfbdc191cda710b7d40501
Kanye09 <kanybek09@yahoo.com>
2020-08-14 19:48:20 -0400

    intro-to-ja hw2-hw3-hw4-done

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,21 @@
 // Your JS code is here
+
+
+/*
+2. The Age Calculator
+Forgot how old someone is? Calculate it!
+
+Store the current year in a variable.
+Store their birth year in a variable.
+Calculate their 2 possible ages based on the stored values.
+Output them to the screen like so: "They are either NN or NN", substituting the values
+ */
+
+
+var curYear = 2020;
+var birthYear = 1993;
+
+var calcAge = curYear - birthYear;
+var johnAge = "John is " + calcAge + " years old.";
+
+console.log(johnAge);
