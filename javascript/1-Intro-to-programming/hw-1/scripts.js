b6f39d390bc23cdc0b8bf8340f5cd4ec6f6a9ad8
kanybekd <duisheev.kanybek@gmail.com>
2020-08-13 20:57:14 -0500

    'hw1intro!'

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,10 @@
 // Your JS code is here
+// Your JS code is her
+
+var num_of_children=2;
+var partner_name='Clinton';
+var geo_loc='Austin';
+var job_title='Engineer';
+
+document.write('You will be an '+ job_title + ' in '+ geo_loc + ',' + ' and married to '+partner_name+' with ',num_of_children+' kids'  )
+console.log('hello)
