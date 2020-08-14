a8982069189984bee79aaf7ed6ed095cb078e955
Farkhat Saliyev <saliyev.farkhat@gmail.com>
2020-08-13 18:09:23 -0700

    intro-to-js

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,8 @@
 // Your JS code is here
+var numberOfchildren = 2;
+    var partnerName = "Aizhan";
+    var location = "San Diego";
+    var jobTitle = "Full Stack Engineer";
+    var fortune ="You will be a"+" "+jobTitle+" in"+" "+location+" and married to"+" "+partnerName+" with"+" "+numberOfchildren+" kids.";
+
+    console.log(fortune)
