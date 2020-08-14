8da0ca74f4cbb441eb5f887ac1ffe5b623c25f58
Viktoriia Leseiko <ViktoriiaL.@Viktoriias-Air.attlocal.net>
2020-08-13 20:45:25 -0500

    intro-to-js-Vikysha1-completed

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+var celsius = 29;
+var celciusInFahrenheit = (celsius*9) /5 + 32;
+console.log(celsius + '°C is ' + celciusInFahrenheit + '°F');
+var fahrenheit = 95;
+var fahrenheitInCelsius = ((fahrenheit-32) *5) /9;
+console.log(fahrenheit + '°F is ' + fahrenheitInCelsius + '°C');
--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,4 @@
 // Your JS code is here
+var fahrenheit = (Math.random() * 100 + 0);
+var fahrenheitInCelsius = ((fahrenheit-32) *5) /9;
+console.log( 'It is ' + fahrenheit + '°F today. That is ' + fahrenheitInCelsius + '°C' )
