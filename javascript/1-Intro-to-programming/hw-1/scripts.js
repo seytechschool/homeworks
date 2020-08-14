e2e6062e0c1b9aaad8c617d5e4c63446c15c31b7
Salidat <hamilova.salidat@gmail.com>
2020-08-13 18:57:43 -0700

    intro-to-js-salidat

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -5,4 +5,4 @@ var nameOfPartner= 'John Smith ';
 var location= ' Australia ';
 var jobTitle=  ' Singer ';
 var future = ' You will be a' +jobTitle+ 'in' +location+  ', married to ' +nameOfPartner+ '  and have '  +numberOfChildren+ ' kids. '
-console.log(future);
+console.log (future);
