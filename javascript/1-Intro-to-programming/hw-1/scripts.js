08976bb09be40e6a169b6d712e96a00120916b1b
Salidat <hamilova.salidat@gmail.com>
2020-08-13 18:52:03 -0700

    new attempt

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,8 @@
 // Your JS code is here
+
+var numberOfChildren= 3 ;
+var nameOfPartner= 'John Smith ';
+var location= ' Australia ';
+var jobTitle=  ' Singer ';
+var future = ' You will be a' +jobTitle+ 'in' +location+  ', married to ' +nameOfPartner+ '  and have '  +numberOfChildren+ ' kids. '
+console.log(future);
