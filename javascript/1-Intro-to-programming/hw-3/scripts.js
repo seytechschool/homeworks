8e8383d9137f61b7105d5d585e9347fed6bad696
Begaiym Bolotbekova <begaiym.bolotbekova@gmail.com>
2020-08-14 03:32:51 +0200

    homeworks intro to pragramming is done

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,11 @@
 // Your JS code is here
+
+var celsius = 15;
+var fahrenheit = 20; 
+var celsiusToFahrenheit = celsius * 9 / 5 + 32;
+
+var fahrenheitToCelsius = (fahrenheit - 32) * 5 / 9;
+
+console.log(celsius + '\u00B0C' + ' is ' + celsiusToFahrenheit.toFixed(2) + '\u00B0F');
+
+console.log(fahrenheit + '\u00B0F' + ' is ' + fahrenheitToCelsius.toFixed(2) + '\u00B0C');
--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,19 @@
 // Your JS code is here
+
+
+//var celsius = 15;
+var fahrenheit = Math.random() * 100; 
+
+/*
+var celsiusToFahrenheit = celsius * 9 / 5 + 32;
+*/
+
+
+var fahrenheitToCelsius = (fahrenheit - 32) * 5 / 9;
+
+
+/*
+console.log(celsius + '\u00B0C' + ' is ' + celsiusToFahrenheit.toFixed(2) + '\u00B0F');
+*/
+
+console.log(fahrenheit.toFixed(2) + '\u00B0F' + ' is ' + fahrenheitToCelsius.toFixed(2) + '\u00B0C');
