02224a1e0fb2f2d39c450f36a75a3471950cfb0d
Nurzhan Nursultanov <nursultanov.nurzhan@gmail.com>
2020-08-14 13:46:59 +1200

    Completed HW1-4 of intro-to-js

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,20 @@
 // Your JS code is here
+function convertToF(degC) {
+  var convertedTemp = (degC * 9) / 5 + 32;
+  var outputString = degC + '°C' + ' is ' + convertedTemp + '°F';
+  console.log(outputString);
+}
+
+// testing functions fromToC
+// convertToF(20) should return 20°C is 68°F
+convertToF(20);
+
+function convertToC(degF) {
+  var convertedTemp = ((degF - 32) * 5) / 9;
+  var outputString = degF + '°F' + ' is ' + convertedTemp + '°C';
+  console.log(outputString);
+}
+
+// testing functions fromToC
+// convertToF(20) should return 68°C is 20°C
+convertToC(68);
