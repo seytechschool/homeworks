8f6bd6f8f72cf2682bd461e4435217ebe7fac9c6
Aidana Khankel <aidanakhankeldieva91@gmail.com>
2020-08-13 20:26:56 -0500

    hm2

--- javascript/2-functions/hw-2/scripts.js
+++ javascript/2-functions/hw-2/scripts.js
@@ -1,1 +1,9 @@
 // Your JS code is here
+
+var currentYear = 2020;
+var yearOfBirth = 1991;
+
+var currentAge = currentYear - yearOfBirth;
+var currentAgePre = currentYear - yearOfBirth-1;
+
+console.log(`I'm ${currentAge} years old or ${currentAgePre}`);
