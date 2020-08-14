ad3bc65401da9aa8d81ce7df9c55205ecd52afe2
Viktoriia Leseiko <ViktoriiaL.@Viktoriias-Air.attlocal.net>
2020-08-13 20:24:56 -0500

    functions Vikysha1

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,8 @@
 // Your JS code is here
+function tellFortune(numOfChildren, partnerName, location, jobTitle){
+    console.log('You will be a ${jobTitle} in ${location}, and married to a {partnerName} with $ {numbOfChildren} kids,');
+}
+
+tellFortune(1, "model", "Los Angeles", "business man");
+tellFortune(3, "bartender", "Chicago", "diplomat");
+tellFortune(5, "accountant", "Italy", "business owner");
