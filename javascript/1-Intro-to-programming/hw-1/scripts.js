99e31ee67a7a3e68c9800e3f79043e4f71c65d6d
Kanykei Kydykbekova <kanykeica@gmail.com>
2020-08-13 18:31:58 -0700

    intro-to-js

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,6 @@
-// Your JS code is here
+var numberOfChildren = 4;
+var partnerName = 'Tom Fox';
+var geoLocation = 'Los Angeles';
+var jobTitle = 'Front-End Developer';
+var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' + partnerName + ' with ' +  numberOfChildren + 'kids';
+console.log(future);
--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,6 @@
-// Your JS code is here
+var inputCelsius = 30;
+var outputFahrenheit = (inputCelsius * 9)/5 + 32;
+console.log(inputCelsius + '\u00B0 Celsius is ' + outputFahrenheit + '\u00B0 Fahrenheit');
+var inputFahrenheit = 86;
+var outputCelsius = (inputFahrenheit -32)/9 * 5;
+console.log(inputFahrenheit + '\u00B0 Fahrenheit is ' + outputCelsius + '\u00B0 Celsius');
