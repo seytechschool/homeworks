29008306c4fe8d15516de9f6210d1e87a9964d9a
Azamat Gaparov <pennphil.ttzf@gmail.com>
2020-08-13 22:14:21 -0400

    funtions hw-3 - completed

--- javascript/2-functions/hw-3/scripts.js
+++ javascript/2-functions/hw-3/scripts.js
@@ -1,1 +1,16 @@
 // Your JS code is here
+// Your JS code is here
+function pluralize(number,word){
+    if(word == 'sheep' || word == 'geese'){
+      return number + ' ' + word
+    }
+    else {
+      return number + ' ' + word + 's'
+    }
+  }
+  
+  pluralize(5, 'cat');
+  pluralize(7, 'turtle');
+  pluralize(10, 'sheep');
+  pluralize(4, 'geese');
+  
