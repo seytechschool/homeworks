7430b1aa0f95c7b38f87921e82b7e21f2dd11933
Azamat Gaparov <pennphil.ttzf@gmail.com>
2020-08-13 21:06:28 -0400

    intro to js hw-2 - completed

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,9 @@
 // Your JS code is here
+// Your JS code is here
+var current_year = 2020;
+var birth_year = 1986;
+
+var age1 = current_year - birth_year;
+var age2 = current_year - birth_year - 1;
+
+console.log('They are either ' + age2 + ' or ' + age1);
