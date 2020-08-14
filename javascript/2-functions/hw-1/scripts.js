b9022426ca220f1f40e02e6dabe0ae116281118d
Askat B <Askat@MacBook-Pro-2.home>
2020-08-13 21:51:03 -0400

    hw-1 functions

--- javascript/2-functions/hw-1/scripts.js
+++ javascript/2-functions/hw-1/scripts.js
@@ -1,1 +1,20 @@
 // Your JS code is here
+function tecllFortune(numOfKids, partnerName, geoLocation, jobTitle) {
+  var p = document.createElement("p");
+  p.innerHTML =
+    "You will be a " +
+    jobTitle +
+    " in " +
+    geoLocation +
+    ", and married to " +
+    partnerName +
+    " with " +
+    numOfKids +
+    " kids.";
+  document.body.appendChild(p);
+}
+
+tecllFortune(2, "Joe", "Italy", "teacher");
+tecllFortune(3, "Jane", "Paris", "dancer");
+tecllFortune(1, "Mary", "Miami", "singer");
+tecllFortune(4, "Bill", "Texas", "actor");
