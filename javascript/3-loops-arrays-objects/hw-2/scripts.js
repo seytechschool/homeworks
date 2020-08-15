16749f739b49d936bc9586a37bbac19ecb43086e
tal-dev <talgat.abykeev@gmail.com>
2020-08-15 12:19:02 -0500

    loops-arrays-objects-tal-dev

--- javascript/3-loops-arrays-objects/hw-2/scripts.js
+++ javascript/3-loops-arrays-objects/hw-2/scripts.js
@@ -1,1 +1,22 @@
 // Your JS code is here
+var companies = ['Nasa', 'SpaceX','Tesla Motors','Nasa', 'SpaceX','Tesla Motors','Nasa', 'SpaceX','Tesla Motors','Nasa', 'SpaceX', 'Twitter'];
+function ordinal_suffix_of(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+function topChoice1(companies) {
+    for(let i = 0; i < companies.length; i++) {
+        console.log(`My ${ordinal_suffix_of(i+1)} company is ${companies[i]}`)
+    }
+}
+topChoice1(companies);
--- javascript/3-loops-arrays-objects/hw-3/scripts.js
+++ javascript/3-loops-arrays-objects/hw-3/scripts.js
@@ -1,1 +1,29 @@
 // Your JS code is here
+var book = [
+    { 
+    title : "Harry Potter and the Philosopher's Stone",
+    author : "J. K. Rowling",
+    alreadyRead : true
+    },
+
+    { 
+    title : "The Hobbit",
+    author : "J.R.R. Tolkien",
+    alreadyRead : false
+    },
+
+    { 
+    title : "11/22/63",
+    author : "Stephen king",
+    alreadyRead : true
+    }
+];
+
+for(let i = 0; i < book.length; i++) {
+    if(book[i].alreadyRead == true) {
+        console.log('You already read "' + book[i].title + '" by ' + book[i].author); 
+    }
+    else {
+        console.log('You still need to read "' + book[i].title + '" by ' + book[i].author); 
+    }   
+}
