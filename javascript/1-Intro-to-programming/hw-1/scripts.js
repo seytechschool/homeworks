131faa1ed2bd3ebf232bbc0dd4755bd7ef5bc610
Umut Muratbekova <umka.muratbekova@gmail.com>
2020-08-16 13:32:28 +0200

    intro-to-js-UmutMuratbekova

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,13 @@
 // Your JS code is here
+// Your JS code is here
+var numChildren = Math.random() * (5);
+var age = (Math.random() * (100-23) + 23);
+var name = "Jacke";
+var place = "Earth";
+
+var nAge = parseInt(age);
+var nKid = parseInt(numChildren)
+
+
+console.log("I am Megan and I am " + nAge + " years old and I have " 
+            + nKid + " kids and I am married to " + name + " And, we live on " + place + " !!!");
