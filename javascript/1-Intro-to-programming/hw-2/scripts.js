a706a550a24c9421fd2291faaabcc4b7894d1d0b
Umut Muratbekova <umka.muratbekova@gmail.com>
2020-08-16 20:12:34 +0200

    intro-to-js-UmutMuratbekova

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+var currentYear = 2020;
+var birthYear = 1991;
+var age = (currentYear - birthYear);
+
+console.log("The person is whether " + age + " years old or " + (age-1) + " years old!!!")
+// Your JS code is here
