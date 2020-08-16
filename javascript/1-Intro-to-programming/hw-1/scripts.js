9f5798c64ab97a469252f70245a118c94e68758f
Max Burkanov <ostu.student@gmail.com>
2020-08-16 14:05:39 -0400

    intro-to-js-maxburkanov

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,6 @@
 // Your JS code is here
+var year = 2020;
+var birth = 2008;
+var age = year - birth;
+var secondAge = age - 1;
+console.log(`They are either ${age} or ${secondAge}`)
--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,15 @@
 // Your JS code is here
+let c = 40;
+let f = c * 9 / 5 + 32;
+console.log(`${c}\u00B0C is ${f}\u00B0F `)
+console.log(`${f}\u00B0F is ${(f - 32) * 5 / 9}\u00B0C `)
+
+
+
+
+function converter(c, f) {
+  console.log(`${c}\u00B0C is ${c*9/5+32}\u00B0F `)  
+
+  console.log(`${f}\u00B0F is ${(f - 32) * 5 / 9}\u00B0C `)
+}
+converter(40, 103)
