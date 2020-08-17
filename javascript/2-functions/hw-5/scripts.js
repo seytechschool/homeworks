bf5942c0b479b040440e205b6272ff6cdfa122d1
Kumar Samsaliev <kumar.samsaliev@gmail.com>
2020-08-17 13:59:45 -0500

    hw-functions, 1-5 homeworks were added

--- javascript/2-functions/hw-5/scripts.js
+++ javascript/2-functions/hw-5/scripts.js
@@ -1,1 +1,7 @@
-// Your JS code is here
+//hw-2-functions-5
+function mixUp(string1, string2){
+    return string2.slice(0,2) + string1.slice(2) + ' ' + string1.slice(0,2) + string2.slice(2);
+  }
+  
+  console.log(mixUp('mix', 'pod'));
+  console.log(mixUp('dog', 'dinner'));
