7d27eaf5c070120ad2f2b2112f7b606d40b4e18a
Kamila Kebere <camilakebere@gmail.com>
2020-08-16 19:47:49 -0500

    3 and 4 homework

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,5 @@
 // Your JS code is here
+var fahrenheitTemp = Math.random();
+var farToCel = (fahrenheitTemp-32)*5/9;
+var symbol = "\u00B0";
+console.log("It is " + fahrenheitTemp + symbol + "today. That's " + farToCel + symbol );
