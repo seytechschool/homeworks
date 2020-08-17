555888074f67b510049c5e372cfbe50744b60266
Kanye09 <kanybek09@yahoo.com>
2020-08-16 23:48:09 -0400

    fonctions hw1-6-is done

--- javascript/2-functions/hw-1/scripts.js
+++ javascript/2-functions/hw-1/scripts.js
@@ -1,1 +1,21 @@
 // Your JS code is here
+
+
+// Let's turn one of the "Intro To Programming" Exercises into a function.
+
+// Write a function named tellFortune that:
+// takes 4 arguments: number of children, partner's name, geographic location, job title.
+// outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+// Call that function 3 times with 3 different values for the arguments.
+
+
+function tellFortune(childNum, name, geoLoc, job) {
+    console.log("You will be a " + job + " in " + geoLoc + ", and married to " + name + " with " + childNum + " kids.");
+}
+
+tellFortune(3, 'Kim Kardashyan)', 'California', 'engineer');
+tellFortune(2, 'Beyonce', 'New York', 'Actor');
+tellFortune(4, 'Rihanna', 'Barbados', 'farmer');
+
+
+
--- javascript/2-functions/hw-6/scripts.js
+++ javascript/2-functions/hw-6/scripts.js
@@ -1,1 +1,30 @@
 // Your JS code is here
+
+
+/**
+ * Challenge Question: String Manipulation
+If you feel comfortable with the other exercises, it's time to expand your knowledge! 
+These puzzles involve manipulating strings; to try them out, you'll need to use some of the built-in JavaScript methods for strings. 
+Methods are pre-written functions that are built into the language.
+
+These questions are not as straightforward as the others. 
+They challenge you to really think like a programmer. Really try to solve them before you peek at the answer.
+
+FixStart
+Create a function called fixStart. It should take a single argument, 
+a string, and return a version where all occurrences of its first character have been replaced with '*', 
+except for the first character itself. You can assume that the string is at least one character long. For example:
+
+fixStart('babble'): 'ba**le'
+fixStart('turtle'): 'tur*le'
+ */
+
+function fixStart(word) {
+    var a = word.charAt(0);
+    return a + word.slice(1).replace(new RegExp(a, 'g'), '*');
+  } 
+
+  fixStart('babble');
+//   fixStart('turtle');
+
+
