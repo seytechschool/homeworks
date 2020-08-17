a14bc1c233dfe0fafd9957bfdb1add841df78dc2
Askat B <Askat@MacBook-Pro-2.home>
2020-08-16 21:59:29 -0400

    hw-4 functions

--- javascript/2-functions/hw-4/scripts.js
+++ javascript/2-functions/hw-4/scripts.js
@@ -1,1 +1,46 @@
 // Your JS code is here
+function squareNumber(num) {
+  var square = num * num;
+  console.log(
+    "The result of squaring the number " + num + " is " + square + "."
+  );
+  return square;
+}
+
+// squareNumber(3);
+
+function halfNumber(num) {
+  var half = num / 2;
+  console.log("Half of " + num + " is " + half + ".");
+  return half;
+}
+
+// halfNumber(40);
+
+function percentOf(num1, num2) {
+  var percent = ((num1 / num2) * 100).toFixed(2);
+  console.log(num1 + " is " + percent + "% of " + num2);
+  return percent;
+}
+
+// percentOf(10, 100);
+
+function areaOfCircle(radius) {
+  var area = (radius * radius * Math.PI).toFixed(2);
+  console.log(
+    "The area for a circle with radius " + radius + " is " + area + "."
+  );
+  return area;
+}
+
+// areaOfCircle(2)
+
+function calc(num) {
+  var halfNum = halfNumber(num);
+  var squareNum = squareNumber(halfNum);
+  var areaCircle = areaOfCircle(squareNum);
+  var percentage = percentOf(areaCircle, squareNumber(areaCircle));
+  return percentage;
+}
+
+// calc(3);
