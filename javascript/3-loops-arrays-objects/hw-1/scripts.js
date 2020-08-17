2cfcb5601447bf04e546db7bbcdc0daca07c49fa
Max Burkanov <ostu.student@gmail.com>
2020-08-16 20:36:20 -0400

    loopsArraysObjects-maxburkanov

--- javascript/3-loops-arrays-objects/hw-1/scripts.js
+++ javascript/3-loops-arrays-objects/hw-1/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+function oddOrEven(n) {
+  for (let i = 0; i <= n; i++) {
+    i % 2 == 0? console.log(`${i} is even`) : console.log(`${i} is odd`)
+  }
+}
+oddOrEven(20)
--- javascript/3-loops-arrays-objects/hw-2/scripts.js
+++ javascript/3-loops-arrays-objects/hw-2/scripts.js
@@ -1,1 +1,31 @@
 // Your JS code is here
+function topChoice(arr) {
+  let st = 'st';
+  let nd = 'nd';
+  let rd = 'rd';
+  let th = 'th';
+  let count = 1;
+  for (let i = 0; i < arr.length; i++) {
+    
+
+    if (count % 10 == 1 && count !== 11) {
+      console.log(`My ${count+st} choice is ${arr[i]}`)
+    
+    }
+    else if (count%10 == 2 && count !==12) {
+      console.log(`My ${count+nd} choice is ${arr[i]}`)
+    } 
+    else if (count%10 == 3 && count !==13) {
+      console.log(`My ${count+rd} choice is ${arr[i]}`)
+    } 
+    else {
+      console.log(`My ${count+th} choice is ${arr[i]}`)
+    }
+
+    count++
+
+  }
+}
+topChoice(
+  ['Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple','Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple','Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple','Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple','Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple','Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple','Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple','Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple','Google', 'Facebook', 'Microsoft', 'Amazon', 'Netflix', 'Apple']
+)
