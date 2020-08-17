fdd2f27e521acbdb7e2a9f920fdf54fb96d11d42
Askat B <Askat@MacBook-Pro-2.home>
2020-08-16 23:42:44 -0400

    hw-2 loops-arrays-objects

--- javascript/3-loops-arrays-objects/hw-2/scripts.js
+++ javascript/3-loops-arrays-objects/hw-2/scripts.js
@@ -1,1 +1,14 @@
 // Your JS code is here
+var topChoices = ["blue", "bike", "apple", "tiger", "pepsi", "summer"];
+
+for (var i = 0; i < topChoices.length; i++) {
+  if (i + 1 === 1) {
+    console.log("My " + (i + 1) + "st choice is " + topChoices[i]);
+  } else if (i + 1 === 2) {
+    console.log("My " + (i + 1) + "nd choice is " + topChoices[i]);
+  } else if (i + 1 === 3) {
+    console.log("My " + (i + 1) + "rd choice is " + topChoices[i]);
+  } else if (i + 1 > 3) {
+    console.log("My " + (i + 1) + "th choice is " + topChoices[i]);
+  }
+}
