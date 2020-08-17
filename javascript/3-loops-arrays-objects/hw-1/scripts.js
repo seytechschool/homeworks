5d725c628900cc5a044f962cd4b11692f632dbe8
Kanye09 <kanybek09@yahoo.com>
2020-08-17 17:03:03 -0400

    3-loops-arrays-objects hw-1 hw-2 hw-3

--- javascript/3-loops-arrays-objects/hw-1/scripts.js
+++ javascript/3-loops-arrays-objects/hw-1/scripts.js
@@ -1,1 +1,27 @@
 // Your JS code is here
+
+
+/**
+ * 1. Even/Odd Counter
+Write a for loop that will iterate from 0 to 20. 
+For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even")
+ */
+
+
+    for (var i = 0; i <= 20; i++) {
+        if (i%2 === 0) {
+            console.log(i + " is even number.");
+        }else if (i === 0) {
+            console.log(i + " is even number.");
+        }else {
+            console.log(i + " is odd number.")
+        }
+    }
+
+
+
+
+
+        
+
+
