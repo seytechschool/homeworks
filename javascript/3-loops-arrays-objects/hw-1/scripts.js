f25a591c915a22448fa971d3e7d02fd3b7278e14
maxburkanov <40704457+maxburkanov@users.noreply.github.com>
2020-08-16 23:01:43 -0400

    Update scripts.js

--- javascript/3-loops-arrays-objects/hw-1/scripts.js
+++ javascript/3-loops-arrays-objects/hw-1/scripts.js
@@ -1,7 +1,7 @@
 // Your JS code is here
 function oddOrEven(n) {
   for (let i = 0; i <= n; i++) {
-    i % 2 == 0? console.log(`${i} is even`) : console.log(`${i} is odd`)
+   console.log( i % 2 == 0? `${i} is even` : `${i} is odd` )
   }
 }
 oddOrEven(20)
