03a7dab738d396073f8d8d94edf7ca7bb5bc7efa
Askat B <Askat@MacBook-Pro-2.home>
2020-08-16 22:46:25 -0400

    hw-6 functions

--- javascript/2-functions/hw-6/scripts.js
+++ javascript/2-functions/hw-6/scripts.js
@@ -1,1 +1,17 @@
 // Your JS code is here
+function fixStart(str) {
+  var firstChar = str.slice(0, 1);
+  var result = firstChar;
+  for (var char of str.slice(1)) {
+    if (char === firstChar) {
+      result += "*";
+    } else {
+      result += char;
+    }
+  }
+  return result;
+}
+
+fixStart("babble");
+fixStart("turtle");
+fixStart("aaa");
