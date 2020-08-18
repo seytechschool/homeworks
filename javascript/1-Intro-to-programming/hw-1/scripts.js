becd1e9254eb6598e0988270c93af5f45ecb48d8
Karlygach Shaiykova <kshaiykova@gmail.com>
2020-08-18 01:40:57 -0700

    add fortune teller project of intro to programming class

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,6 @@
-// Your JS code is here
+var children = 5;
+var partner = 'Kaku';
+var city = 'Redmond';
+var job ='Software Engineer';
+console.log('You will be a ' + job + ' in ' + city + ' and married to ' + partner +' with '+ children);
+ 
