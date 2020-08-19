99a032e68357963823d8c9bd384410d2471ff9cd
Atay Djunusaliev <adjunusaliev@gmail.com>
2020-08-19 09:27:27 -0700

    functions-hw1-hw5

--- javascript/2-functions/hw-3/scripts.js
+++ javascript/2-functions/hw-3/scripts.js
@@ -1,1 +1,17 @@
 // Your JS code is here
+function pluralize (number, animal){
+    if (animal != 'sheep' && animal != 'geese')
+       { 
+         return number + ' ' + animal + 's';
+       }
+    else { 
+      return number + ' ' + animal;
+      }
+    }
+    
+    console.log('I have ' + pluralize(10,'sheep'));
+    console.log('I have ' + pluralize(5, 'geese'));
+    console.log('I have ' + pluralize(1, 'cat'));
+    console.log('I have ' + pluralize(2, 'dog'));
+
+    
--- javascript/2-functions/hw-5/scripts.js
+++ javascript/2-functions/hw-5/scripts.js
@@ -1,1 +1,14 @@
 // Your JS code is here
+// ### MixUp
+
+// Create a function called `mixUp`. It should take in two strings, and return the concatenation of the two strings (separated by a space) slicing out and swapping the first 2 characters of each. You can assume that the strings are at least 2 characters long. For example:
+
+// ```
+// mixUp('mix', 'pod'): 'pox mid'
+// mixUp('dog', 'dinner'): 'dig donner'
+
+function mixUp(word1, word2) {
+    return word2.slice(0, 2) + word1.slice(2) + " " + word1.slice(0, 2) + word2.slice(2);
+  }
+  console.log(mixUp('mix', 'pod'));
+  console.log(mixUp('dog', 'dinner'));
