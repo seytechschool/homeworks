0a8f0a4281c954397620cde12da500c9a31754d2
Rustam Ermatov <rustam.22.97@gmail.com>
2020-08-20 12:12:45 +0600

    hw1

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,6 @@
-// Your JS code is here
+var currentYear = 2020;
+var birthYear = 1997;
+var age = (currentYear - birthYear);
+
+
+console.log("A person is either " + age + " years old or " + (age-1) + " years old !!!")
--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,6 @@
-// Your JS code is here
+var randomF = Math.random() * 100;
+console.log("Random Farenheit  number is: " + randomF.toFixed(2) + '\u00B0');
+var cel = (randomF - 32) * 5 / 9;
+
+
+console.log("It is going to be: " + cel.toFixed(2) +'\u00B0' + " Celsius " )
