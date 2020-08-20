5595b30c928c6c27391dc2cc6a82ea791cac04b9
Rustam Ermatov <rustam.22.97@gmail.com>
2020-08-21 00:35:00 +0600

    hw2

--- javascript/2-functions/hw-2/scripts.js
+++ javascript/2-functions/hw-2/scripts.js
@@ -1,1 +1,16 @@
-// Your JS code is here
+function compareTwo(num1, num2){
+  if(typeof(num1) != 'number' || typeof(num2) != 'number'){
+    console.log(`Impossible to compare ${num1} and ${num2}`)
+  }else if(num1 === num2){
+    console.log('Both numbers are equal')
+  }else if(num1>num2){
+    console.log(`${num1} is bigger than ${num2}`)
+  }else{
+    console.log(`${num2} is bigger than ${num1}`)
+  }
+}
+
+compareTwo(4, 20);
+compareTwo(14, 14);
+compareTwo('apple', 20);
+
--- javascript/2-functions/hw-4/scripts.js
+++ javascript/2-functions/hw-4/scripts.js
@@ -1,1 +1,24 @@
-// Your JS code is here
+function squareNumber(num){
+	return num * num;
+}
+
+function halfNumber(num){
+	return num / 2;
+}
+
+function percentOf(num1, num2){
+	return (num1 / num2 * 100).toFixed(2);
+}
+
+function areaOfCircle(r){
+	return Math.PI.toFixed(2) * r * r;
+}
+
+
+console.log(squareNumber(1) + " is a squared number");
+console.log(halfNumber(4) + " is a half of " + 4);
+console.log(areaOfCircle(2) + " is a square of circle with radius of 2");
+console.log(percentOf(3, areaOfCircle(2)) + " is a percentage of 3 out of area of circle");
+
+
+
