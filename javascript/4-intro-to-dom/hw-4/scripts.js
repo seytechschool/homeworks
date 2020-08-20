1a1cbc3eee232f26ea1d08081b1e31767c55db52
Azamat Gaparov <pennphil.ttzf@gmail.com>
2020-08-20 04:12:57 -0400

    intro to dom hw-4 - completed

--- javascript/4-intro-to-dom/hw-4/scripts.js
+++ javascript/4-intro-to-dom/hw-4/scripts.js
@@ -1,1 +1,17 @@
 // Your JS code is here
+// Write a function that takes a certain type of tag and counts the 
+// number of elements with that tag. The function should return 
+// "There a X tags of type y on the page. For example:
+
+// ```
+// countTags('p'): 'There are 3 tags of type p on the page'
+// ```
+
+
+function countTags(param){
+    var elementTypeCount = document.getElementsByTagName(param);
+    // console.log(elementTypeCount);
+    console.log('There are ' + elementTypeCount.length + ' tags of type ' + param + ' on the page');
+}
+
+countTags('p');
