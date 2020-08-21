8606250eedd833ebf08f1e0b5c7317fcb0b93a92
Rustam Ermatov <rustam.22.97@gmail.com>
2020-08-21 16:57:56 +0600

    hw3

--- javascript/3-loops-arrays-objects/hw-2/scripts.js
+++ javascript/3-loops-arrays-objects/hw-2/scripts.js
@@ -1,1 +1,4 @@
-// Your JS code is here
+var arr = ['Messi', 'CR7', "Ronaldo", 'Raul', 'Z.Zidan'];
+ for(var i = 0; i<arr.length; i++){
+     console.log("My #" + (i+1) + " favorite soccer player is " + arr[i])
+ }
--- javascript/3-loops-arrays-objects/hw-3/scripts.js
+++ javascript/3-loops-arrays-objects/hw-3/scripts.js
@@ -1,1 +1,38 @@
-// Your JS code is here
+var arr = [
+    {
+      title: "Martin Eden ",
+      year: 1880,
+      alreadyRead: true
+    }, 
+    {
+      title: "Samurai",
+      year: 1990,
+      alreadyRead:  true
+    }, 
+    {
+      title: "Romeo Julietta",
+      year: 1880,
+      alreadyRead: false
+    }, 
+    {
+      title: "War and Peace",
+      year: 1880,
+      alreadyRead: true
+    }, 
+  ];
+  
+  for(var i = 0; i<arr.length; i++){
+    if(arr[i].alreadyRead){
+      console.log("One of my favorite books: ");
+       console.log(arr[i].title + " is from " + " " + arr[i].year);
+       console.log("");
+    }else{
+      console.log("One of my planned books to read: ")
+     console.log(arr[i].title + " is from " + " " + arr[i].year)
+       console.log("");
+  
+    }
+  }
+  
+    
+   
