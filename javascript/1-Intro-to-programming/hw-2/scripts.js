d198fba28e1eddd12272943cbd6af78be59d547b
Marat Gaip <marattig@gmail.com>
2020-08-23 02:00:58 -0700

    Revert "Functions kanybekd"

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,5 +1,1 @@
-
-var c_year=2020;
-var b_year=1987;
-
-console.log('They are either ',c_year-b_year,' or ',b_year-c_year);// Your JS code is here
+// Your JS code is here
--- javascript/2-functions/hw-5/scripts.js
+++ javascript/2-functions/hw-5/scripts.js
@@ -1,6 +1,1 @@
 // Your JS code is here
-function mixUp(a,b){
-  console.log(b.slice(0,2)+a.slice(2,),a.slice(0,2)+b.slice(2,))
-}
-mixUp('dog','dinner')
-// Your JS code is here
