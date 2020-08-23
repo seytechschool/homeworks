ffbd0384729ce1ef1a449c9b5b5e39d2b39daa51
Azamat Gaparov <pennphil.ttzf@gmail.com>
2020-08-23 08:06:40 -0400

    functions hw-3 - updated

--- javascript/2-functions/hw-3/scripts.js
+++ javascript/2-functions/hw-3/scripts.js
@@ -1,14 +1,14 @@
 // Your JS code is here
 // Your JS code is here
 function pluralize(number,word){
-    if(word == 'sheep' || word == 'geese'){
-      return number + ' ' + word
-    }
-    else {
-      return number + ' ' + word + 's'
-    }
+  var zeroPluralWords = ['deer', 'geese', 'moose', 'sheep', 'elk', 'walrus', 'antelope', 'fish', 'buffalo', 'salmon'];
+  if(zeroPluralWords.includes(word) || number === 1){
+    return number + ' ' + word;
   }
-  
+  else {
+    return number + ' ' + word + 's'
+  }
+}
   pluralize(5, 'cat');
   pluralize(7, 'turtle');
   pluralize(10, 'sheep');
