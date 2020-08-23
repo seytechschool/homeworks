024c8cbf8482e7451715e2ad326e18dd1d1b3afc
Aijanava <68876609+Aijanava@users.noreply.github.com>
2020-08-23 14:17:30 -0400

    Update scripts.js

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+var celsius = 29;
+var celsiusInF = 84;
+console.log(celsius + '°C is ' + celsiusInF + '°F');
+var fahrenheit = 79;
+var fahrenheitInC = 29;
+console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
