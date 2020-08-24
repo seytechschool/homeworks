f43effd81a79bed24c21a46b12ae0e8cafe4a906
Atay Djunusaliev <adjunusaliev@gmail.com>
2020-08-23 21:36:54 -0700

    loops-arrays-objects hw1-hw3

--- javascript/3-loops-arrays-objects/hw-2/scripts.js
+++ javascript/3-loops-arrays-objects/hw-2/scripts.js
@@ -1,1 +1,14 @@
 // Your JS code is here
+function myChoice() {
+    var topCompanyChoices = ['Google', 'Apple', 'Netflix']
+    var suffix1 = 'st';
+    var suffix2 = 'nd';
+     var suffix3 = 'rd';
+    
+  console.log('My 1' + suffix1 + ' choice is ' + topCompanyChoices.slice(0, 1));
+  console.log('My 2' + suffix2 + ' choice is ' + topCompanyChoices.slice(1, 2));
+  console.log('My 3' + suffix3 + ' choice is ' + topCompanyChoices.slice(2, 3));
+  }
+  myChoice()
+
+  //Completed without Bonus 
