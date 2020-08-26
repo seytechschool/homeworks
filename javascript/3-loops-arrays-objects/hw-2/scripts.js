05572a6c8982aa6fa5f0c22fd0f098cff0df4051
Ulanbek <ulan312@gmail.com>
2020-08-26 19:18:57 -0400

    loops-arrays-objects-ulanbak

--- javascript/3-loops-arrays-objects/hw-2/scripts.js
+++ javascript/3-loops-arrays-objects/hw-2/scripts.js
@@ -1,1 +1,20 @@
 // Your JS code is here
+var topChoices = ['gray', 'John F Kennedy', 'Arsenal F.C.', 'Abraham Lincoln' ];
+for(var i = 0; i<topChoices.length; i++){
+  firstLog = `My # ${i+1} choice is ${topChoices[i]}`
+  console.log(firstLog);
+  if(i==0){
+    console.log(`My ${i+1}st ${firstLog.slice(6)}`);
+  }
+  else if(i==1){
+    console.log(`My ${i+1}nd ${firstLog.slice(6)}`);
+  }
+  else if(i==2){
+    console.log(`My ${i+1}rd ${firstLog.slice(6)}`);
+  }
+  else if(i==3){
+    console.log(`My ${i+1}th ${firstLog.slice(6)}`);
+  }
+} ;
+
+
--- javascript/3-loops-arrays-objects/hw-3/scripts.js
+++ javascript/3-loops-arrays-objects/hw-3/scripts.js
@@ -1,1 +1,17 @@
 // Your JS code is here
+var books = [
+  {title: 'To Kill a Mockingbird', author: 'Harper Lee', alreadyRead:true},
+  {title: 'The Remains of the Day', author: 'Kazuo Ishiguro', alreadyRead:true},
+  {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', alreadyRead:false}
+];
+for (var i=0; i<books.length; i++){
+  var book = books[i];
+  var firstLog = (book.title + ' by ' + book.author);
+  console.log(firstLog);
+  if(book.alreadyRead==true){
+    console.log('You already read '+firstLog);
+  }
+  else if(book.alreadyRead==false) {
+  console.log('You still need to read '+ firstLog);
+  }
+}
