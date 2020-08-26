4b2824e3a3535f5bb727dabfac013694a2e21cde
Ulanbek <ulan312@gmail.com>
2020-08-26 19:07:31 -0400

    last two homeworks

--- javascript/2-functions/hw-5/scripts.js
+++ javascript/2-functions/hw-5/scripts.js
@@ -1,1 +1,8 @@
 // Your JS code is here
+function mixUp(str1,str2){
+ var mix = str2.slice(0,2) + str1.slice(2) +
+ ' ' + str1.slice(0,2)+str2.slice(2) ;
+  console.log(mix);
+}
+mixUp('mix', 'pod');
+mixUp('dog','dinner');
--- javascript/2-functions/hw-6/scripts.js
+++ javascript/2-functions/hw-6/scripts.js
@@ -1,1 +1,7 @@
 // Your JS code is here
+function fixStart(str){
+  var firstChar = str.charAt(0);
+  return firstChar + str.slice(1).split(firstChar).join('*');
+}
+console.log(fixStart('babble'));
+console.log(fixStart('turtle'));
