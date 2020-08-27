0657913e4de70d0aaaa34817195313c38131bf36
Farkhat Saliyev <saliyev.farkhat@gmail.com>
2020-08-26 21:56:28 -0700

    functions

--- javascript/3-loops-arrays-objects/hw-3/scripts.js
+++ javascript/3-loops-arrays-objects/hw-3/scripts.js
@@ -1,9 +1,1 @@
 // Your JS code is here
-var array = ['The Hobbit by J.R.R. Tolkien', 'The Prince by Niccolo Machiavelli,', 'Sapiens: A Brief History of Humankind by Yuval Noah Harari', 
-'The Kite Runner by Khaled Hosseini', 'The Journey Home: Autobiography of an American Swami by Radhanath Swami']
-
-
-
-
-
-
