78715fa4d95dbe2f21d224d606fffca734bf8caf
Kanye09 <kanybek09@yahoo.com>
2020-08-27 14:32:27 -0400

    intro-to-dom-kanye09

--- javascript/4-intro-to-dom/hw-4/scripts.js
+++ javascript/4-intro-to-dom/hw-4/scripts.js
@@ -1,1 +1,9 @@
 // Your JS code is here
+
+function countTags(tag) {
+    var getTags = document.querySelectorAll(tag);
+  console.log('There are '  + getTags.length + ' tags of type ' + tag + ' on the page.');
+  
+  }
+  
+  countTags('p');
