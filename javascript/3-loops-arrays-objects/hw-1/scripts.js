1e6fc71496e08bf0afc90552a83b2061a0b2bb9c
Farkhat Saliyev <saliyev.farkhat@gmail.com>
2020-08-28 15:47:43 -0700

    hw Loops

--- javascript/3-loops-arrays-objects/hw-1/scripts.js
+++ javascript/3-loops-arrays-objects/hw-1/scripts.js
@@ -1,1 +1,15 @@
 // Your JS code is here
+function number (){
+    for (i = 0; i <= 20; i ++){
+        if (i === 0 ) {
+          console.log (i + ' is even');
+        }
+        else if (i % 2 === 0) {
+          console.log (i + ' is even');
+        }
+        else {
+          console.log (i + ' is odd');
+        }
+    }
+    }
+    number()
--- javascript/3-loops-arrays-objects/hw-3/scripts.js
+++ javascript/3-loops-arrays-objects/hw-3/scripts.js
@@ -1,1 +1,33 @@
 // Your JS code is here
+var array = [
+    { title:'The Hobbit', 
+      author: 'J.R.R. Tolkien',
+      alreadyRead: false
+    }, 
+    { title:'The Prince',
+      author: 'Niccolo Machiavelli',
+      alreadyRead: true
+    },
+    { title:'Sapiens: A Brief History of Humankind', 
+      author: 'Yuval Noah Harari',
+      alreadyRead: true
+    },
+    { title:'The Kite Runner', 
+      author: 'Khaled Hosseini',
+      alreadyRead: true
+    },
+    { title:'The Titan',
+      author: 'Theodore Dreiser',
+      alreadyRead: false
+    }
+  ]
+  
+   for (i = 0; i < array.length; i++) { 
+    if (array[i].alreadyRead){
+      console.log('You already read: '+ array[i].title + " " + array[i].author);
+    }
+    else{
+      console.log('You still need to read: '+ array[i].title +' '+array[i].author)
+    }
+    console.log()
+   }
