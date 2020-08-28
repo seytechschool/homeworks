ba410d9b16dcbed87dbc714ad81067f063074373
Farkhat Saliyev <saliyev.farkhat@gmail.com>
2020-08-28 15:55:29 -0700

    DOM_FarkhatSaliyev

--- javascript/4-intro-to-dom/hw-2/scripts.js
+++ javascript/4-intro-to-dom/hw-2/scripts.js
@@ -1,1 +1,23 @@
 // Your JS code is here
+      document.body.style.fontFamily = "Arial, sans-serif";
+
+      var nickname = document.querySelector('#nickname');
+      nickname.textContent = "Farkhat";
+
+      var favorites = document.querySelector('#favorites');
+      favorites.textContent = "Travel";
+
+      var hometown = document.querySelector('#hometown');
+      hometown.textContent = "Almaty";
+
+      var item = document.querySelectorAll('li')
+        for (i = 0; i < item.length; i++) {
+          item[i].className = 'listitem'
+        }
+        for(i = 0; i < document.querySelectorAll('.listitem').length; i++){
+          document.querySelectorAll('.listitem')[i].style.color = 'red';
+        }
+
+      var img = document.createElement('img');
+      img.src = 'https://cdn.civitatis.com/kazajstan/almaty/almaty.jpg';
+      document.body.appendChild(img);
