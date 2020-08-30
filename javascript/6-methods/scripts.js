fa5bcb9d2c8eac45676d423b4f62f74f88425a0f
Kanye09 <kanybek09@yahoo.com>
2020-08-29 22:48:05 -0400

    methods-homework

--- javascript/6-methods/scripts.js
+++ javascript/6-methods/scripts.js
@@ -1,1 +1,28 @@
 // Your JS code is here
+
+
+
+//1. Display the position of the first occurrence of "World" in the variable txt
+
+var txt = "Hello World"; 
+var a = txt.indexOf('World');
+console.log(a);
+
+
+//2. Use the splice() method to remove "Orange" and "Apple" from fruits.
+
+var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+var removeItem = fruits.splice(2, 2);
+
+console.log(fruits);
+console.log(removeItem);     
+
+
+//3. Use the concat() method to concatenate girls and boys.
+
+var girls = ["Cecilie", "Lone"];
+var boys = ["Emil", "Tobias", "Linus"];  
+
+var girlsAndBoys = girls.concat(boys);
+
+console.log(girlsAndBoys);
