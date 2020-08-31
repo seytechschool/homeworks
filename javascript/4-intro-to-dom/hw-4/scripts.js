186ffbfb57b1d077436332ddf5b8777e31185b62
Nurzhan Nursultanov <nursultanov.nurzhan@gmail.com>
2020-09-01 00:59:47 +1200

    Completed HW 1-4 of dom

--- javascript/4-intro-to-dom/hw-4/scripts.js
+++ javascript/4-intro-to-dom/hw-4/scripts.js
@@ -1,1 +1,8 @@
 // Your JS code is here
+function countTags(tagName) {
+  var allTags = document.querySelectorAll(tagName);
+  var result = `There are ${allTags.length} tags of type ${tagName} on the page`;
+  return result;
+}
+
+console.log(countTags('div'));
