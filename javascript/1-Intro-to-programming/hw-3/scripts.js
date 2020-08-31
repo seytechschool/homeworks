d0c8d5b18c0eeb5af8784064eaa51a3ca3614765
Aziz Abdullaev <aliqsson@gmail.com>
2020-08-30 22:02:16 -0500

    finished hw-1

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,7 @@
-// Your JS code is here
+var celciusTemp = 17;
+var fahrenheitFromCelsius = (celciusTemp * 9 / 5 + 32).toFixed(2);
+console.log(celciusTemp,'\u00B0C is', fahrenheitFromCelsius,'\u00B0F');
+
+var fahrenheitTemp = 69;
+var celsiusFromFahrenheit = ((fahrenheitTemp - 32) * 5 / 9).toFixed(2);
+console.log(fahrenheitTemp,'\u00B0F is', celsiusFromFahrenheit,'\u00B0C');
--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,3 @@
-// Your JS code is here
+var fahrenheitTemp = Math.round(Math.random()*100);
+var celsiusFromFahrenheit = ((fahrenheitTemp - 32) * 5 / 9).toFixed(2);
+console.log('It\'s', fahrenheitTemp,'\u00B0F today. That\'s', celsiusFromFahrenheit,'\u00B0C.');
