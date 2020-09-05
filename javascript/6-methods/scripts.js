80126006561b43985d6ba15663aa723fcbd0ccf0
Nurzhan Nursultanov <nursultanov.nurzhan@gmail.com>
2020-09-05 21:11:37 +1200

    Completed HW of methods

--- javascript/6-methods/scripts.js
+++ javascript/6-methods/scripts.js
@@ -1,1 +1,35 @@
-// Your JS code is here
+// Display the position of the first occurrence of "World" in the variable txt
+
+var txt = 'Hello World';
+console.log(txt.indexOf('World'));
+
+// Use the splice() method to remove "Orange" and "Apple" from fruits.
+
+var fruits = ['Banana', 'Orange', 'Apple', 'Kiwi'];
+fruits.splice(1, 2);
+console.log(fruits);
+
+// Use the concat() method to concatenate girls and boys.
+
+var girls = ['Cecilie', 'Lone'];
+var boys = ['Emil', 'Tobias', 'Linus'];
+
+console.log(girls.concat(boys));
+
+// Create an object called "User". Give any 4 properties(key, values) to it.
+
+var user = {
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 25,
+  city: 'Los Angeles',
+};
+
+var allKeys = Object.keys(user);
+var allValues = Object.values(user);
+console.log(allKeys);
+console.log(allValues);
+
+for (aKey in user) {
+  console.log(`${aKey} : ${user[aKey]}`);
+}
