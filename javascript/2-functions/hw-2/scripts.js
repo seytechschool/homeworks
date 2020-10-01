c95966acf21caf185b8bd5b68ea8330865fb172f
Aijanava <68876609+Aijanava@users.noreply.github.com>
2020-10-01 12:36:01 -0400

    Update scripts.js

--- javascript/2-functions/hw-2/scripts.js
+++ javascript/2-functions/hw-2/scripts.js
@@ -1,1 +1,14 @@
-// Your JS code is here
+function largerNum(num1, num2){
+ if(num1 === num2){
+   console.log('Equal numbers');
+ } else if(num1>num2){
+   return num1;
+ } else if(num2>num1){
+   return num2;
+ }
+}
+
+console.log(largerNum(5,5));
+console.log(largerNum(10,-50));
+console.log(largerNum(3,8));
+
