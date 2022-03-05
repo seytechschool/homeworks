d7c121a95b19d3d0e312a29b35ba1389fbb74081
Ilgiz Temi <temirbekov91@gmail.com>
2022-03-04 23:34:01 -0500

    @ilgiztemi-fortune-teller-js

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,1 +1,6 @@
 // Your JS code is here
+let children = 'N';
+let partner = 'Z';
+let geoLocation = 'Y';
+let jobTitle = 'X';
+console.log("You will be a " + jobTitle + " in " + geoLocation + ", and married to " + partner + " with " + children + " kids.");
