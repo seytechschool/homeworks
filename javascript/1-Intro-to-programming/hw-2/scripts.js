131c2361ae279c9c0644539143ae751649903217
Ilgiz Temi <temirbekov91@gmail.com>
2022-03-04 23:39:18 -0500

    @ilgiztemi-age-calculator-js

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,6 @@
 // Your JS code is here
+let currentYear = 2022;
+let birthYear = 1991;
+let possibleAge1 = currentYear - birthYear;
+let possibleAge2 = (currentYear - birthYear) - 1;
+console.log("They are either " + possibleAge1 + " or " + possibleAge2);
