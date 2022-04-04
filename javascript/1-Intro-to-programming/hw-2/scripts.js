0a926c59be57314d3d3105bba61a700a99ee9c23
meerimbatyr <batyrkanovam@gmail.com>
2022-04-05 00:27:25 +0300

    age calculator hw

--- javascript/1-Intro-to-programming/hw-2/scripts.js
+++ javascript/1-Intro-to-programming/hw-2/scripts.js
@@ -1,1 +1,6 @@
-// Your JS code is here
+var currentYear = 2022;
+var birthYear = 1993;
+var possibleAge1 = currentYear-birthYear;
+var possibleAge2 = possibleAge1-1;
+
+console.log('They are either ' + possibleAge1 + ' or ' + possibleAge2+ ', substituting the values.');
