c435e86339e40e428ab2bbf240fa7d67be3f63fa
meerimbatyr <batyrkanovam@gmail.com>
2022-04-05 01:38:46 +0300

    set temperature to random number and to be integer

--- javascript/1-Intro-to-programming/hw-4/scripts.js
+++ javascript/1-Intro-to-programming/hw-4/scripts.js
@@ -1,1 +1,6 @@
-// Your JS code is here
+
+var fahrenheit = Math.floor(Math.random()*100);
+var fahrenheitToCelsius = Math.floor((fahrenheit-32) * 5 / 9);
+
+
+console.log('It is ' +fahrenheit + '\u00B0 F today. That is ' + fahrenheitToCelsius + '\u00B0 C.');
