3b9422e2c5c59597ebc15ab90934e0d1c9302647
Nurzada <nurzadabakytkyzy8@gmail.com>
2022-10-13 11:01:52 -0700

    add homework

--- javascript/1-Intro-to-programming/hw-1/scripts.js
+++ javascript/1-Intro-to-programming/hw-1/scripts.js
@@ -1,8 +1,8 @@
 // Your JS code is here
 
 let children = 2;
-let partner = "David"
-let country = "USA"
-let job = "Devops"
+let partner = "David";
+let country = "USA";
+let job = "Devops";
 
 alert(`You will be a ${job} in ${country}, and married to ${partner} with ${children} kids.`)
