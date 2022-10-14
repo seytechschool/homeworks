56b3a49b4bf2e5f9caddc9f9b365da5bba536150
Nurzada <nurzadabakytkyzy8@gmail.com>
2022-10-14 11:17:59 -0700

    h/w3

--- javascript/1-Intro-to-programming/hw-3/scripts.js
+++ javascript/1-Intro-to-programming/hw-3/scripts.js
@@ -1,1 +1,11 @@
 // Your JS code is here
+
+let temperature=7;
+let fahrenteit=temperature*9/5+32; 
+let celsius=(fahrenteit-32)*5/9;
+
+alert(`${temperature}\u00B0 C is ${fahrenteit}\u00B0 F, ${fahrenteit}\u00B0 F is ${celsius}\u00B0 C`) ; 
+
+
+
+
